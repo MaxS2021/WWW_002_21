e290fb871106c102e2af03204629666fce297dcf
solovov.ma <maxvmd@mail.ru>
2022-02-10 10:14:10 +0300

    mars2 bootstarp

--- mars2.py
+++ mars2.py
@@ -26,7 +26,7 @@ def promotion():
 
 @app.route('/image_mars')
 def image_mars():
-    filename = "img/mars-123.jpg"
+    filename = "img/mars1.jpg"
     return f"""<!doctype html>
                 <html lang="en">
                   <head>
@@ -42,6 +42,42 @@ def image_mars():
            
                   </body>
                 </html>"""
+
+
+@app.route('/promotion_image')
+def promotion_image():
+    filename = "img/mars1.jpg"
+    return f"""<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                        <link rel="stylesheet" 
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                        crossorigin="anonymous">
+                        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                        <title>Привет, Марс!</title>
+                      </head>
+                      <body>
+                        <h1>Жди нас, Марс!</h1>
+                        <img src="{url_for('static', filename=filename)}" 
+                                alt="здесь должна была быть картинка, но не нашлась">'''
+                        <h2>Планета Марс.</h2> 
+                        <div class="alert alert-primary" role="alert">
+                            Человечество вырастет когда-нибудь, наверное, мы надеемся очень, из детства!
+                        </div>
+                        <div class="alert alert-success" role="alert">
+                            Человечеству мало одной планеты!
+                        </div>
+                        <div class="alert alert-danger" role="alert">
+                            Мы сделаем обитаемыми безжизненные пока планеты.
+                        </div>
+                        <div class="alert alert-warning" role="alert">
+                            И начнем с Марса!
+                        </div>
+                      </body>
+                    </html>"""
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
